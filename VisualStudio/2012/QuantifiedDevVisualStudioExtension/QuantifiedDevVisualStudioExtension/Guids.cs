527e0137840b1974a3bb3615953edc2152ecebc1
edsykes <ed.sykes.public@gmail.com>
2014-03-31 19:39:01 +0100

    Added the visual studio client

---
+++ VisualStudio/2012/QuantifiedDevVisualStudioExtension/QuantifiedDevVisualStudioExtension/Guids.cs
@@ -0,0 +1,17 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace QuantifiedDev.QuantifiedDevVisualStudioExtension
+{
+    static class GuidList
+    {
+        public const string guidQuantifiedDevVisualStudioExtensionPkgString = "b8e9e30c-7b19-455b-a83b-33a81be4e0f5";
+        public const string guidQuantifiedDevVisualStudioExtensionCmdSetString = "41f7014c-a43c-4b8c-ae55-270bd9608ba9";
+        public const string guidToolWindowPersistanceString = "8dfc24d0-676e-418c-815d-6d1b18d892f5";
+        public const string guidQuantifiedDevVisualStudioExtensionEditorFactoryString = "0d4dd946-ff9c-46e1-86e5-6e9052d3c9ea";
+
+        public static readonly Guid guidQuantifiedDevVisualStudioExtensionCmdSet = new Guid(guidQuantifiedDevVisualStudioExtensionCmdSetString);
+        public static readonly Guid guidQuantifiedDevVisualStudioExtensionEditorFactory = new Guid(guidQuantifiedDevVisualStudioExtensionEditorFactoryString);
+    };
+}
---
+++ VisualStudio/2012/QuantifiedDevVisualStudioExtension/QuantifiedDevVisualStudioExtension/QuantifiedDevVisualStudioExtensionPackage.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Net.Http.Headers;
+using System.Runtime.InteropServices;
+using System.ComponentModel.Design;
+using EnvDTE;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.Shell;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
+
+namespace QuantifiedDev.QuantifiedDevVisualStudioExtension
+{
+    /// <summary>
+    /// This is the class that implements the package exposed by this assembly.
+    ///
+    /// The minimum requirement for a class to be considered a valid package for Visual Studio
+    /// is to implement the IVsPackage interface and register itself with the shell.
+    /// This package uses the helper classes defined inside the Managed Package Framework (MPF)
+    /// to do it: it derives from the Package class that provides the implementation of the 
+    /// IVsPackage interface and uses the registration attributes defined in the framework to 
+    /// register itself and its components with the shell.
+    /// </summary>
+    // This attribute tells the PkgDef creation utility (CreatePkgDef.exe) that this class is
+    // a package.
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    // This attribute is used to register the information needed to show this package
+    // in the Help/About dialog of Visual Studio.
+    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
+    // This attribute is needed to let the shell know that this package exposes some menus.
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    // This attribute registers a tool window exposed by this package.
+    [ProvideToolWindow(typeof(MyToolWindow))]
+    [ProvideEditorExtension(typeof(EditorFactory), ".quantifieddevvisualstudioextension", 50, 
+              ProjectGuid = "{A2FE74E1-B743-11d0-AE1A-00A0C90FFFC3}", 
+              TemplateDir = "Templates", 
+              NameResourceID = 105,
+              DefaultName = "QuantifiedDevVisualStudioExtension")]
+    [ProvideKeyBindingTable(GuidList.guidQuantifiedDevVisualStudioExtensionEditorFactoryString, 102)]
+    // Our Editor supports Find and Replace therefore we need to declare support for LOGVIEWID_TextView.
+    // This attribute declares that your EditorPane class implements IVsCodeWindow interface
+    // used to navigate to find results from a "Find in Files" type of operation.
+    [ProvideEditorLogicalView(typeof(EditorFactory), VSConstants.LOGVIEWID.TextView_string)]
+    [Guid(GuidList.guidQuantifiedDevVisualStudioExtensionPkgString)]
+    [ProvideAutoLoadAttribute("{F1536EF8-92EC-443C-9ED7-FDADF150DA82}")]
+    public sealed class QuantifiedDevVisualStudioExtensionPackage : Package
+    {
+        private bool buildSucceeded;
+
+        /// <summary>
+        /// Default constructor of the package.
+        /// Inside this method you can place any initialization code that does not require 
+        /// any Visual Studio service because at this point the package object is created but 
+        /// not sited yet inside Visual Studio environment. The place to do all the other 
+        /// initialization is the Initialize method.
+        /// </summary>
+        public QuantifiedDevVisualStudioExtensionPackage()
+        {
+// ReSharper disable RedundantStringFormatCall
+            Debug.WriteLine(string.Format(CultureInfo.CurrentCulture, "Entering constructor for: {0}", ToString()));
+// ReSharper restore RedundantStringFormatCall
+        }
+
+        /// <summary>
+        /// This function is called when the user clicks the menu item that shows the 
+        /// tool window. See the Initialize method to see how the menu item is associated to 
+        /// this function using the OleMenuCommandService service and the MenuCommand class.
+        /// </summary>
+        private void ShowToolWindow(object sender, EventArgs e)
+        {
+            // Get the instance number 0 of this tool window. This window is single instance so this instance
+            // is actually the only one.
+            // The last flag is set to true so that if the tool window does not exists it will be created.
+            ToolWindowPane window = FindToolWindow(typeof(MyToolWindow), 0, true);
+            if ((null == window) || (null == window.Frame))
+            {
+                throw new NotSupportedException(Resources.CanNotCreateWindow);
+            }
+            var windowFrame = (IVsWindowFrame)window.Frame;
+            ErrorHandler.ThrowOnFailure(windowFrame.Show());
+        }
+
+
+        /////////////////////////////////////////////////////////////////////////////
+        // Overridden Package Implementation
+        #region Package Members
+
+        /// <summary>
+        /// Initialization of the package; this method is called right after the package is sited, so this is the place
+        /// where you can put all the initialization code that rely on services provided by VisualStudio.
+        /// </summary>
+        protected override void Initialize()
+        {
+            Debug.WriteLine (CultureInfo.CurrentCulture.ToString(), "Entering Initialize() of: {0}", this);
+            base.Initialize();
+
+            //Create Editor Factory. Note that the base Package class will call Dispose on it.
+            RegisterEditorFactory(new EditorFactory(this));
+
+            // Add our command handlers for menu (commands must exist in the .vsct file)
+            var mcs = GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+            if ( null != mcs )
+            {
+                // Create the command for the menu item.
+                var menuCommandId = new CommandID(GuidList.guidQuantifiedDevVisualStudioExtensionCmdSet, (int)PkgCmdIDList.cmdidQuantifiedDev);
+                var menuItem = new MenuCommand(MenuItemCallback, menuCommandId );
+                mcs.AddCommand( menuItem );
+                // Create the command for the tool window
+                var toolwndCommandId = new CommandID(GuidList.guidQuantifiedDevVisualStudioExtensionCmdSet, (int)PkgCmdIDList.cmdidQuantifiedDevTool);
+                var menuToolWin = new MenuCommand(ShowToolWindow, toolwndCommandId);
+                mcs.AddCommand( menuToolWin );
+            }
+
+            InitializeSendingBuildEvents();
+        }
+        #endregion
+
+        /// <summary>
+        /// This function is the callback used to execute a command when the a menu item is clicked.
+        /// See the Initialize method to see how the menu item is associated to this function using
+        /// the OleMenuCommandService service and the MenuCommand class.
+        /// </summary>
+        private void MenuItemCallback(object sender, EventArgs e)
+        {
+            // Show a Message Box to prove we were here
+            var uiShell = (IVsUIShell)GetService(typeof(SVsUIShell));
+            Guid clsid = Guid.Empty;
+            int result;
+            ErrorHandler.ThrowOnFailure(uiShell.ShowMessageBox(
+                       0,
+                       ref clsid,
+                       "QuantifiedDevVisualStudioExtension",
+                       string.Format(CultureInfo.CurrentCulture, "Inside {0}.MenuItemCallback()", ToString()),
+                       string.Empty,
+                       0,
+                       OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                       OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST,
+                       OLEMSGICON.OLEMSGICON_INFO,
+                       0,        // false
+                       out result));
+        }
+
+        private void InitializeSendingBuildEvents()
+        {
+            Debug.WriteLine(CultureInfo.CurrentCulture.ToString(), "Entering Initialize() of: {0}", this);
+         
+            const string context = "SYKES";
+
+            if (Settings.Default.StreamId == "")
+            {
+                CreateStream();
+            }
+
+            var dte = (DTE)GetService(typeof(DTE));
+            dte.Events.BuildEvents.OnBuildBegin += (scope, action) =>
+            {
+                buildSucceeded = true;
+                SendBuildEvent(context, scope, action, new object[] { "Build", "Start" }, new JObject());
+            };
+
+            dte.Events.BuildEvents.OnBuildDone += (scope, action) =>
+            {
+                var properties = new JObject();
+                properties["Result"] = buildSucceeded ? "Success" : "Failure";
+                SendBuildEvent(context, scope, action, new object[] { "Build", "Finish" }, properties);
+            };
+
+            dte.Events.BuildEvents.OnBuildProjConfigDone += (project, config, platform, solutionConfig, success) =>
+            {
+                Debug.WriteLine("Project Build Begin", context);
+                Debug.WriteLine(project, context);
+                Debug.WriteLine(config, context);
+                Debug.WriteLine(platform, context);
+                Debug.WriteLine(solutionConfig, context);
+                Debug.WriteLine(success, context);
+                buildSucceeded &= success;
+            };
+
+
+        }
+
+        private static void SendBuildEvent(string context, vsBuildScope scope, vsBuildAction action, object[] actionTags, JObject properties)
+        {
+            var buildActionName = actionTags[0] + " " + actionTags[1];
+            Debug.WriteLine(buildActionName, context);
+
+            var streamId = Settings.Default.StreamId;
+            var token = Settings.Default.WriteToken;
+
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", token);
+            var buildEvent = new JObject();
+            buildEvent["dateTime"] = DateTime.UtcNow.ToString("o");
+
+            var location = new JObject();
+            location["lat"] = 52;
+            location["long"] = 0;
+            buildEvent["location"] = location;
+
+            buildEvent["actionTags"] = new JArray(actionTags);
+            buildEvent["objectTags"] = new JArray(new object[] { "Computer", "Software" });
+
+            properties["Language"] = "C#";
+            properties["Environment"] = "VisualStudio2012";
+            buildEvent["properties"] = properties;
+
+            var url = string.Format("http://quantifieddev.herokuapp.com/stream/{0}/event", streamId);
+            var content = new StringContent(buildEvent.ToString(Newtonsoft.Json.Formatting.None));
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+            client.PostAsync(url, content).ContinueWith(postTask =>
+            {
+                Debug.WriteLine(postTask.Result.StatusCode.ToString(), context, buildActionName);
+                Debug.WriteLine(scope.ToString(), context, buildActionName);
+                Debug.WriteLine(action.ToString(), context, buildActionName);
+            });
+        }
+
+        private static void CreateStream()
+        {
+            var client = new HttpClient();
+            var result = client.PostAsync("http://quantifieddev.herokuapp.com/stream", new StringContent("{}")).Result;
+            var streamDetails = result.Content.ReadAsStringAsync().Result;
+            var jsonStream = JObject.Parse(streamDetails);
+            Settings.Default.StreamId = jsonStream.GetValue("streamid").ToString();
+            Settings.Default.WriteToken= jsonStream.GetValue("writeToken").ToString();
+            Settings.Default.ReadToken= jsonStream.GetValue("readToken").ToString();
+            Settings.Default.Save();
+            Debug.WriteLine(streamDetails);
+        }
+
+    }
+}
