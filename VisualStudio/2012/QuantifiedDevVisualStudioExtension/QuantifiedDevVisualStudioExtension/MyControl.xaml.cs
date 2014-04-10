123b1002b055b8a65f2beda8edcabcf7ad311c18
edsykes <ed.sykes.public@gmail.com>
2014-04-10 19:34:45 +0100

    Added streamid and read token to the gui

--- VisualStudio/2012/QuantifiedDevVisualStudioExtension/QuantifiedDevVisualStudioExtension/MyControl.xaml.cs
+++ VisualStudio/2012/QuantifiedDevVisualStudioExtension/QuantifiedDevVisualStudioExtension/MyControl.xaml.cs
@@ -1,36 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Globalization;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace QuantifiedDev.QuantifiedDevVisualStudioExtension
 {
     /// <summary>
     /// Interaction logic for MyControl.xaml
     /// </summary>
-    public partial class MyControl : UserControl
+    public partial class MyControl
     {
         public MyControl()
         {
             InitializeComponent();
-            latitude.Text = Settings.Default.Latitude.ToString();
-            longitude.Text = Settings.Default.Longitude.ToString();
-        }
-
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Globalization", "CA1300:SpecifyMessageBoxOptions")]
-        private void button1_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show(string.Format(System.Globalization.CultureInfo.CurrentUICulture, "We are inside {0}.button1_Click()", this.ToString()),
-                            "QuantifiedDevTool");
+            latitude.Text = Settings.Default.Latitude.ToString(CultureInfo.InvariantCulture);
+            longitude.Text = Settings.Default.Longitude.ToString(CultureInfo.InvariantCulture);
+            streamId.Text = Settings.Default.StreamId;
+            readToken.Text = Settings.Default.ReadToken;
 
         }
 
