d86d5b2c5cda359502a6c3c3873a8c2f7f6e5887
edsykes <ed.sykes.public@gmail.com>
2015-03-26 10:23:34 +0000

    rename

--- VisualStudio/allversions/QuantifiedDevVisualStudioExtension/QuantifiedDevVisualStudioExtension/Properties/AssemblyInfo.cs
+++ VisualStudio/allversions/QuantifiedDevVisualStudioExtension/QuantifiedDevVisualStudioExtension/Properties/AssemblyInfo.cs
@@ -29,8 +29,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
 
 [assembly: InternalsVisibleTo("1selfVisualStudioExtension_IntegrationTests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100018d299a12e946ef865f08ded5b61c5a79abe9d9e3de300a93dfd3dc4fd10eaf4fc32e291998a631440a43013714cf20dc58d0aed094c372813b9847ee7716b3d7c4bc06782edd758b482e87410449298f34c31e5b1e88e57a24d3b8ce8f6c2b954734bbe0cf8db1896ae0c99cc90e42e4b78c3105b93bffc3539a77ee24eacc")]
 [assembly: InternalsVisibleTo("1selfVisualStudioExtension_UnitTests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100018d299a12e946ef865f08ded5b61c5a79abe9d9e3de300a93dfd3dc4fd10eaf4fc32e291998a631440a43013714cf20dc58d0aed094c372813b9847ee7716b3d7c4bc06782edd758b482e87410449298f34c31e5b1e88e57a24d3b8ce8f6c2b954734bbe0cf8db1896ae0c99cc90e42e4b78c3105b93bffc3539a77ee24eacc")]
